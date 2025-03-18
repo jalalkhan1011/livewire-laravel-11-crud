3541c95ce419c912214d4f707630d75d2652f9e5
jalalkhan1011 <jalal.khan981@gmail.com>
2025-03-17 18:54:51 -0800

    project update

--- resources/views/livewire/product/sale/product-sale-component.blade.php
+++ resources/views/livewire/product/sale/product-sale-component.blade.php
@@ -55,7 +55,7 @@
                 @include('livewire.product.sale.create')
             @endif
             @if ($isEditModalOpen)
-            @include('livewire.product.sale.edit')
+                @include('livewire.product.sale.edit')
             @endif
         </div>
     </div>
